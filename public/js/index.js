d91595bee84f5893f3c4766222e0df789479ba7a
Natali Hreshchyshyn <natalihreshchyshyn@gmail.com>
2018-06-13 13:47:32 +0000

    Initial

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,25 @@
-// document.querySelector('.navbar-toggler')
-//     .addEventListener('click', function() {
-//         if (document.querySelector('.top').style.display == "") {
-//             document.querySelector('.top').style.display = "none";
-//         } else {
-//             document.querySelector('.top').style.display = "";
-//         }
-//     });
-
 $(function() {
     $('.navbar-nav>li>a').on('click', function() {
         $('.navbar-collapse').collapse('hide');
     });
 });
+
+$(function() {
+    $('.smooth').on('click', function(event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
+const lastDay = new Date();
+document.querySelector('.last-client-bid')
+  .addEventListener('change', function() {
+  document.querySelector('.time')
+    .innerText = lastDay.setDate(lastDay.getDate() + 7);
+});
+
---
+++ public/js/lotslist.js
@@ -0,0 +1,40 @@
+const lotList = new LotList('lot-list.json', $('.lots-container'));
+class LotList {
+    constructor (lotsUrl, renderContainer) {
+        fetch(lotsURL)
+            .then(result => result.json() )
+            .then(lots => {
+                this.lots = lots;
+                this.renderLots(renderContainer, lots);
+                this.addEventListeners();
+            });
+    }
+    getLotById(id) {
+        return this.lot-list.find(el => el.id === id);
+    }
+    renderLots(container, lots) {
+        let lotListDomString = '';
+        lots.forEach(lot => {
+            lotListDomString += 
+                `<figure>
+                    <img src="img/${lot.image}" alt="${lot.title}">
+                    <figcaption>${lot.title}</figcaption>
+                </figure>`;
+        });
+        container.html(lotListDomString);
+    }
+     addEventListeners() {
+        $('#lotInfoModal').on('show.bs.modal', event => {
+            const button = $(event.relatedTarget); // Button that triggered the modal
+            const id  = String(button.data('id')); // Extract info from data-* attributes
+            const lot = this.getLotById(id);
+            const modal = $('#lotInfoModal');
+            modal.find('.modal-body .card-img-top')
+                .attr('src', 'img/'+lot.image)
+                .attr('alt', lot.title);
+            modal.find('.modal-body .card-title').text(lot.title);
+            modal.find('.modal-body .card-text').text(lot.description);
+        });
+    }
+}
+
