07abda960aebda0deccb201a4ee3148b18d2a63b
Natali Hreshchyshyn <natalihreshchyshyn@gmail.com>
2018-06-15 11:43:24 +0000

    Initial

--- public/js/index-form.js
+++ public/js/index-form.js
@@ -5,7 +5,7 @@ function sendForm(e) {
     const form = document.querySelector('.client-form');
     if (form.checkValidity()) { 
         e.preventDefault();
-        fetch('form', {
+        fetch('/form', {
             headers: {
                 'Content-type': 'application/json'
             },
@@ -13,15 +13,28 @@ function sendForm(e) {
             body: JSON.stringify({
                 name: document.querySelector('.client-form input[name=client-name]').value,
                 contact: document.querySelector('.client-form input[name=client-contact]').value,
-                lot:  document.querySelector('.client-form input[name=lot-number]').value,
+                lot:  document.querySelector('.client-form select[name=lot-number]').value,
                 bid:  document.querySelector('.client-form input[name=client-bid]').value,
             })
         })
         .then( response => response.json() )
         .then( lotsData => {
-         document.getElementById('lot-1');  
-         document.querySelector('.last-client-name').innerText = lotsData.name;
-         document.querySelector('.last-client-bid').innerText = lotsData.bid;
+            if (lotsData.lot === 1) {
+                document.querySelector('.last-client-name1').innerText = lotsData.name;
+                document.querySelector('.last-client-bid1').innerText = lotsData.bid;
+            } else if (lotsData.lot === 2) {
+                document.querySelector('.last-client-name2').innerText = lotsData.name;
+                document.querySelector('.last-client-bid2').innerText = lotsData.bid;    
+            }  else if (lotsData.lot === 3) {
+                document.querySelector('.last-client-name3').innerText = lotsData.name;
+                document.querySelector('.last-client-bid3').innerText = lotsData.bid;    
+            }  else if (lotsData.lot === 4) {
+                document.querySelector('.last-client-name4').innerText = lotsData.name;
+                document.querySelector('.last-client-bid4').innerText = lotsData.bid;    
+            }  else if (lotsData.lot === 5) {
+                document.querySelector('.last-client-name5').innerText = lotsData.name;
+                document.querySelector('.last-client-bid5').innerText = lotsData.bid;    
+            } 
         });
     }
 }
--- public/js/lotslist.js
+++ public/js/lotslist.js
@@ -17,9 +17,22 @@ class LotList {
             lotListDomString += 
                 `<figure>
                     <img src="img/${lot.image}" alt="${lot.title}">
-                    <figcaption>${lot.title}</figcaption>
+                    <figcaption><button type="button" class="btn" data-toggle="modal" data-target="#exampleModal">${lot.title}</button></figcaption>
                 </figure>`;
         });
         container.html(lotListDomString);
+    }
+    addEventListeners() {
+        $('#lotInfoModal').on('show.bs.modal', event => {
+            const button = $(event.relatedTarget); // Button that triggered the modal
+            const id  = String(button.data('id')); // Extract info from data-* attributes
+            const lot = this.getLotById(id);
+            const modal = $('#lotInfoModal');
+            modal.find('.modal-body .lot-img-top')
+                .attr('src', 'img/'+lot.image)
+                .attr('alt', lot.title);
+            modal.find('.modal-body .lot-title').text(lot.title);
+            modal.find('.modal-body lot-text').text(lot.description);
+        });
     }
 }
